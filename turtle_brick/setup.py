55fc175bb8cd9276ac50b19b45b30addf2ee87ae
0nhc <thefoxfoxfox@outlook.com>
2024-10-26 19:19:13 -0500

    add license and config and launch

--- turtle_brick/setup.py
+++ turtle_brick/setup.py
@@ -1,4 +1,6 @@
-from setuptools import setup
+from setuptools import find_packages, setup
+import os
+from glob import glob
 
 package_name = 'turtle_brick'
 
@@ -10,13 +12,15 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+        (os.path.join('share', package_name, 'launch'), glob('launch/*')),
+        (os.path.join('share', package_name, 'config'), glob('config/*')),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
-    maintainer='zhengxiao-han',
-    maintainer_email='thefoxfoxfox@outlook.com',
+    maintainer='Zhengxiao Han',
+    maintainer_email='hanzx@u.northwestern.edu',
     description='TODO: Package description',
-    license='TODO: License declaration',
+    license='WTFPL',
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
